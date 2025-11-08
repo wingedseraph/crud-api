16c520ac2fcf1e26d456dfeae615baa34c1d8601
wingedseraph <r89647@gmail.com>
2025-11-09 00:47:25 +0300

    feat(utils): improve parseBody validaiton

--- src/consts/messages.ts
+++ src/consts/messages.ts
@@ -1,5 +1,6 @@
 export const MESSAGE = {
   NOT_FOUND: 'Not Found',
   METHOD_NOT_ALLOWED: 'Method Not Allowed',
+  INVALID_DATA_TYPE: 'Invalid data type',
   UNKNOWN_ERROR: 'Unknown Error',
 } as const;
--- src/utils/parseBody.ts
+++ src/utils/parseBody.ts
@@ -1,21 +1,33 @@
 export const parseBody = (body: string) => {
   const parsed = JSON.parse(body);
-  const missingFieldsArray: string[] | null = [];
+  const missingFieldsArray: string[] = [];
 
-  if (!parsed.username) {
-    missingFieldsArray.push('username (string)');
-  }
+  const { username, age, hobbies } = parsed;
 
-  if (parsed.age === undefined || parsed.age === null) {
-    missingFieldsArray.push('age (number)');
-  }
+  const validations = [
+    {
+      condition: !parsed || !username,
+      field: 'username (string)',
+    },
+    {
+      condition: age === undefined || age === null || typeof age !== 'number',
+      field: 'age (number)',
+    },
+    {
+      condition: !hobbies || !Array.isArray(hobbies),
+      field: 'hobbies (array of strings)',
+    },
+  ];
 
-  if (!parsed.hobbies) {
-    missingFieldsArray.push('hobbies (array of strings)');
-  }
+  validations.forEach((validation) => {
+    if (validation.condition) {
+      missingFieldsArray.push(validation.field);
+    }
+  });
 
   if (missingFieldsArray.length > 0) {
     return { parsed, missingFieldsArray };
   }
+
   return { parsed, missingFieldsArray: [] };
 };
