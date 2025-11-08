b6e0a4861645596cd74af76f9cbe10cc6adb1857
wingedseraph <r89647@gmail.com>
2025-11-09 02:14:31 +0300

    feat(delete): implement delete request functional

---
+++ src/controllers/delete.ts
@@ -0,0 +1,34 @@
+import { type IncomingMessage, type ServerResponse } from 'http';
+import { users } from '../db';
+import { sendGenericResponse } from '../handler/send-response';
+import { parseId } from '../utils/parseId';
+import { validateUserId } from '../utils/validateUserId';
+
+export const delete_ = (
+  request: IncomingMessage,
+  response: ServerResponse<IncomingMessage>,
+) => {
+  const userId = parseId(request);
+
+  if (userId) {
+    const validation = validateUserId(userId);
+    if (!validation.isValid) {
+      return sendGenericResponse(response, 400, validation.error);
+    }
+
+    const userExist = users.getState().find((user) => user.id === userId);
+    if (!userExist) {
+      return sendGenericResponse(response, 404, `${userId} doesn't exist`);
+    }
+
+    // users.filter((user) => user.id !== userId)
+    users.dispatch({
+      type: 'DELETE_USER',
+      id: userId,
+    });
+
+    return sendGenericResponse(response, 204);
+  }
+
+  sendGenericResponse(response, 404);
+};
