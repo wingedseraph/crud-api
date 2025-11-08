9d1a82fc2b6e6799004a0df7e594ca2be3828348
wingedseraph <r89647@gmail.com>
2025-11-09 00:46:56 +0300

    feat(server): add fallback port

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,14 @@ import {
   type IncomingMessage,
   type ServerResponse,
 } from 'node:http';
-import { sendNotFoundResponse } from './handler/send-response';
+import {
+  sendGenericResponse,
+  sendNotFoundResponse,
+} from './handler/send-response';
 import { route } from './router/router';
+import { MESSAGE } from './consts/messages';
 
-const PORT = process.env.SINGLE_PORT;
+const PORT = process.env.SINGLE_PORT || 4000;
 
 const requestListener = (
   request: IncomingMessage,
@@ -26,7 +30,12 @@ const requestListener = (
       }
     } catch (e) {
       if (e instanceof Error) console.error(e.message);
+      sendGenericResponse(response, 500, MESSAGE.UNKNOWN_ERROR);
     }
+  });
+
+  request.on('error', (err) => {
+    console.error(err);
   });
 };
 
