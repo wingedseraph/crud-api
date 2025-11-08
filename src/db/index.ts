22bf52fb8c220dd57d9866893c786f8827037d27
wingedseraph <r89647@gmail.com>
2025-11-09 02:14:05 +0300

    refactor(db): implement simple store instead of const users[]

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,54 @@
+import { randomUUID } from 'node:crypto';
+
+type GenericReducer<state, action> = (state: state, action: action) => state;
+
 type User = {
   username: string;
   age: number;
-  hobbies?: string[];
+  hobbies: string[];
   id: string;
 };
 
+type UserAction = {
+  type: 'ADD_USER' | 'DELETE_USER' | 'UPDATE_USER';
+  id?: string;
+  user?: User;
+};
+
+const record: GenericReducer<User[], UserAction> = (state: User[] = [], action: UserAction) => {
+
+  switch (action.type) {
+    case 'ADD_USER': {
+      const id = randomUUID();
+      return state.concat([{ id, ...action.user } as User]);
+    }
+
+    case 'DELETE_USER':
+      return state.filter((element) => element.id !== action.id);
+
+    case 'UPDATE_USER':
+      return state;
+
+    default:
+      return state;
+  }
+};
+
+export const createStore = <state, action>(reducer: GenericReducer<state, action>, initialState: state) => {
+  let state: state = initialState;
+
+  const getState = () => state;
+  const dispatch = (action: action) => {
+    state = reducer(state, action);
+    return state;
+  };
+
+  return { getState, dispatch };
+};
+
+
 // todo: remove before push
-export const users: User[] = [
+const initialState: User[] = [
   {
     username: 'good',
     age: 23,
@@ -20,3 +62,5 @@ export const users: User[] = [
     id: 'da0ed38e-eb3d-4d50-864a-30de53a257d6',
   },
 ];
+
+export const users = createStore(record, initialState);
