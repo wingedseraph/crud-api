47c01369ce124225dc7d38cfbfc3b8c9996e8285
wingedseraph <r89647@gmail.com>
2025-11-10 00:20:04 +0300

    fix(worker): sync store once on message

--- src/cluster/workerHandler.ts
+++ src/cluster/workerHandler.ts
@@ -5,62 +5,70 @@ import { styledWorker } from '../server/workerServer';
 import { StoreChange } from './clusterManager';
 
 const styledUser = styleText('yellowBright', 'User');
+let isSyncingFromIPC = false;
 
 export const setupWorkerHandler = () => {
   process.on('message', (message) => {
     const msg = message as StoreChange;
     if (msg.cmd === 'STORE_CHANGE') {
-      switch (msg.action) {
-        case 'ADD_USER':
-          users.dispatch({
-            type: 'ADD_USER_WITH_ID',
-            user: msg.user,
-          });
-          console.log(
-            `[${styledWorker} ${cluster.worker!.id}] store synchronized: ${styledUser} added with ID ${msg.user?.id}`,
-          );
-          break;
+      isSyncingFromIPC = true;
 
-        case 'UPDATE_USER':
-          users.dispatch({
-            type: 'UPDATE_USER',
-            id: msg.id,
-            user: msg.user,
-          });
-          console.log(
-            `[${styledWorker} ${cluster.worker!.id}] store synchronized: ${styledUser} updated with ID ${msg.id}`,
-          );
-          break;
+      try {
+        switch (msg.action) {
+          case 'ADD_USER':
+            users.dispatch({
+              type: 'ADD_USER_WITH_ID',
+              user: msg.user,
+            });
+            console.log(
+              `[${styledWorker} ${cluster.worker!.id}] ${msg.action} store synchronized: ${styledUser} added with ID ${msg.user?.id}`,
+            );
+            break;
 
-        case 'DELETE_USER':
-          users.dispatch({
-            type: 'DELETE_USER',
-            id: msg.id,
-          });
-          console.log(
-            `[${styledWorker} ${cluster.worker!.id}] store synchronized: ${styledUser} deleted with ID ${msg.id}`,
-          );
-          break;
+          case 'UPDATE_USER':
+            users.dispatch({
+              type: 'UPDATE_USER',
+              id: msg.id,
+              user: msg.user,
+            });
+            console.log(
+              `[${styledWorker} ${cluster.worker!.id}] ${msg.action} store synchronized: ${styledUser} updated with ID ${msg.id}`,
+            );
+            break;
+
+          case 'DELETE_USER':
+            users.dispatch({
+              type: 'DELETE_USER',
+              id: msg.id,
+            });
+            console.log(
+              `[${styledWorker} ${cluster.worker!.id}] ${msg.action} store synchronized: ${styledUser} deleted with ID ${msg.id}`,
+            );
+            break;
+        }
+      } finally {
+        isSyncingFromIPC = false;
       }
     }
   });
 
   users.subscribe((newState, action) => {
-    if (process.send) {
-      let messageData = action;
-      if (action.type === 'ADD_USER') {
-        const newUser = newState[newState.length - 1];
-        messageData = { ...action, user: newUser };
-      }
-
-      process.send({
-        cmd: 'STORE_CHANGE',
-        action: messageData.type,
-        id:
-          messageData.id ||
-          (messageData.user ? messageData.user.id : undefined),
-        data: messageData.user,
-      });
+    if (isSyncingFromIPC || !process.send) {
+      return;
     }
+
+    let messageData = action;
+    if (action.type === 'ADD_USER') {
+      const newUser = newState[newState.length - 1];
+      messageData = { ...action, user: newUser };
+    }
+
+    process.send({
+      cmd: 'STORE_CHANGE',
+      action: messageData.type,
+      id:
+        messageData.id || (messageData.user ? messageData.user.id : undefined),
+      user: messageData.user,
+    });
   });
 };
